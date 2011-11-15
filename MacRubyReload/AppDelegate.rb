f8c912b2251acb94b0e4569823053972776d62a0
Jean-Denis Muys <jdmuys@mac.com>
2011-11-15 18:48:31 +0100

    Initial commit

--- MacRubyReload/AppDelegate.rb
+++ MacRubyReload/AppDelegate.rb
@@ -3,13 +3,25 @@
 #  MacRubyReload
 #
 #  Created by Jean-Denis Muys on 11/15/11.
-#  Copyright 2011 Gleamware. All rights reserved.
+#  Copyright 2011 Jean-Denis Muys. All rights reserved.
 #
+# License: attributionware. Simply credit me.
 
 class AppDelegate
     attr_accessor :window
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
     end
+    
+    def doReloadSource(sender)
+        puts "Reloading MacRuby source code\n";
+        reloadMacRubySource;
+    end
+    
+    def doTest(sender)
+        #puts "Hello, Dynamic MacRuby World!\n"
+        puts "Hello, Modified MacRuby World!\n"
+    end
+
 end
 
--- MacRubyReload/rb_main.rb
+++ MacRubyReload/rb_main.rb
@@ -10,6 +10,29 @@
 # do that here too.
 framework 'Cocoa'
 
+# this function is defined in the main Ruby file so that it can avoid reloading it.
+# only from here can we unambiguously construct the name of the main ruby file
+def reloadMacRubySource
+    main = File.basename(__FILE__)
+    
+    dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+    Dir.glob(File.join(dir_path, '*.loc.txt')).each do |loc|
+        
+        #        path = IO.read(loc)
+        File.open(loc, "r") do |file|
+            # currently, location files contain only one line. We loop through all lines anyway for future expansion.
+            file.each_line do |path|
+                path = path.gsub(/\r?\n/, '')
+                
+                pathBase = File.basename(path)
+                if File.basename(path) != main
+                    load(path)
+                end
+            end
+        end
+    end    
+end
+
 # Loading all the Ruby project files.
 main = File.basename(__FILE__, File.extname(__FILE__))
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
